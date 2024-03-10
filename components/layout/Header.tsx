c0b7b572769fda203d14df106307c9aecfc5e7ad
dksifoua <dimitri.sifoua@gmail.com>
2024-03-10 19:31:36 -0400

    Built layout.

---
+++ components/layout/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+  return (
+    <div className="flex flex-row w-full h-[10%] border-b-2">
+      <h1>Header</h1>
+    </div>
+  )
+}
---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+
+  return (
+    <aside className="w-[10%] h-full border-r-2">
+      Sidebar
+    </aside>
+  )
+}
