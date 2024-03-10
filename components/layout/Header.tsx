a4fdb18ffb00cdca2ff7cb6ca8963a968f98c1cf
dksifoua <dimitri.sifoua@gmail.com>
2024-03-10 19:52:09 -0400

    Added theme toggle button.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,32 @@
-import React from "react"
+"use client"
+
+import React, { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Moon, Sun } from "lucide-react"
+
+function ThemeToggle() {
+  const [theme, setTheme] = useState("light")
+
+  return (
+    <Button
+      size="icon"
+      variant="ghost"
+      onClick={() => theme === "dark" ? setTheme("light") : setTheme("dark")}
+      className="rounded-full"
+    >
+      {
+        theme === "dark" ? <Sun/> : <Moon/>
+      }
+    </Button>
+  )
+}
 
 export default function Header() {
   return (
     <div className="flex flex-row w-full h-[10%] border-b-2">
-      <h1>Header</h1>
+      <div className="flex items-center ml-auto">
+        <ThemeToggle/>
+      </div>
     </div>
   )
 }
